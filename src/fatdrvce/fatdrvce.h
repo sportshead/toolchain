a9dd2d1b7f016ffd17d0ae432b713db9f18ac172
mateoconlechuga <matthewwaltzis@gmail.com>
2019-03-15 22:08:44 -0600

    start working on api for fatdrvce

--- src/fatdrvce/fatdrvce.h
+++ src/fatdrvce/fatdrvce.h
@@ -18,10 +18,15 @@
  *
  * void main(void) {
  *     static fat_partition_t fat_partitions[MAX_PARTITIONS];
- *     static uint8_t sector[512];
+ *     static uint8_t buffer[512];
+ *     static uint8_t driver_buffer[512];
  *     static jmp_buf msdenv;
- *     msd_event_t evnt;
+ *     usb_error_t evnt;
  *     uint8_t num;
+ *
+ *     // Set the buffers used by the FAT MSD library
+ *     msd_SetDriverBuffer(driver_buffer);
+ *     msd_SetDataBuffer(buffer);
  *
  *     // Configure USB error handling, e.g. removal before write is finished
  *     if ((evnt = setjmp(msdenv))) {
@@ -33,9 +38,6 @@
  *     if (msd_Init(10000) != 0) {
  *         // No MSD devices found
  *     }
- *
- *     // Set the buffer used by the FAT library
- *     fat_SetBuffer(sector);
  *
  *     // Locate available FAT partitions on the MSD device
  *     if ((num = fat_Find(fat_partitions, MAX_PARTITIONS) == 0) {
@@ -51,6 +53,7 @@
  *     }
  *
  *     // Here is where you can perform any other open/read/write operations
+ *     // The 'buffer' variable can be used for both reading and writing
  *
  *     // Deinitialize FAT and MSD subsystems
  *     fat_Deinit();
@@ -75,10 +78,6 @@ extern "C" {
 #endif
 
 typedef struct {
-    uint8_t dummy;
-} msd_t;
-
-typedef struct {
     uint32_t lba;       /**< Logical Block Address (LBA) of FAT partition */
     uint32_t size;      /**< Size of FAT partition */
 } fat_partition_t;
@@ -101,11 +100,10 @@ typedef struct {
 #define FAT_DIR       (1 << 4)  /**< Entry is a directory (or subdirectory). */
 #define FAT_ALL       (FAT_DIR | FAT_VOLLABEL | FAT_SYSTEM | FAT_HIDDEN | FAT_RDONLY)
 
-typedef enum msd_event {
-    MSD_EVENT_NONE = 0,   /**< No event detected. */
-    MSD_EVENT_DETACHED,   /**< The MSD device was unplugged. */
-    MSD_EVENT_XFER_ERROR  /**< The MSD device encountered a transfer error. */
-} msd_event_t;
+typedef enum msd_config {
+    MSD_CONFIG_NONE = 0,              /**< No extra configuration parameters */
+    MSD_CONFIG_AUTO_ADVANCE_DATA_BUFFER = 1, /**< Automatically advance data buffer pointer by 512 bytes on each read / write. */
+} msd_config_t;
 
 /**
  * Initializes the FAT filesystem and allows other FAT functions to be used.
@@ -114,9 +112,9 @@ typedef enum msd_event {
  * @warning When using the FAT library, calls to os_GetCSC() will cause USB not
  * to work. This will probably be fixed in later revisions, but just use the
  * \c keypadc library for now.
- * @return 0 on success.
+ * @return USB_SUCCESS on success.
  */
-uint8_t fat_Init(void);
+usb_error_t fat_Init(void);
 
 /**
  * Deinitializes the FAT filesystem and frees the handles to the device.
@@ -124,9 +122,9 @@ uint8_t fat_Init(void);
  * function call. It should not be called in a device error handler,
  * only in the case of a graceful exit, otherwise an infinite loop may
  * occur.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void fat_Deinit(void);
+usb_error_t fat_Deinit(void);
 
 /**
  * Locates any available FAT partitions detected on the mass storage device
@@ -134,11 +132,12 @@ void fat_Deinit(void);
  * allocate space for \p result before calling this function. In addition, the
  * \c msd_Find and \c msd_Select functions must be called prior to using this
  * function in order to select any attached MSD.
- * @param result Array of FAT partitions available, returned from function.
  * @param max The maximum number of FAT partitions that can be found.
- * @return The number of found FAT partitions.
+ * @param partitions Array of FAT partitions available, returned from function.
+ * @param num Array of FAT partitions available, returned from function.
+ * @return USB_SUCCESS on success.
  */
-uint8_t fat_Find(fat_partition_t *result, uint8_t max);
+usb_error_t fat_Find(uint8_t max, fat_partition_t *partions, uint8_t *num);
 
 /**
  * Selects which FAT parition the library should use. You can only work with
@@ -148,9 +147,9 @@ uint8_t fat_Find(fat_partition_t *result, uint8_t max);
  * \c fat_Find.
  * @param index Which FAT partition to use, i.e. '0' would select the first
  * partition.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void fat_Select(fat_partition_t *list, uint8_t index);
+usb_error_t fat_Select(fat_partition_t *list, uint8_t index);
 
 /**
  * Opens a file on the device, given an absolute path. Names must use the 8.3
@@ -159,46 +158,41 @@ void fat_Select(fat_partition_t *list, uint8_t index);
  * @param path Absolute 8.3 file path name.
  * @param flags \c FAT_O_WRONLY, \c FAT_O_RDONLY, \c FAT_O_RDWR for write only,
  * read only, and read/write respectively.
- * @return A FAT file descriptor, or a value less than 0 on error.
+ * @param fd Returned file descriptor variable.
+ * @return USB_SUCCESS on success.
  */
-int8_t fat_Open(const char *path, int flags);
+usb_error_t fat_Open(int8_t *fd, const char *path, int flags);
 
 /**
  * Closes and frees any open file descriptor.
  * @param fd File descriptor.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void fat_Close(int8_t fd);
+usb_error_t fat_Close(int8_t fd);
 
 /**
  * Gets the size of a file.
  * @param fd File descriptor.
- * @return File size in bytes.
+ * @param value Returned file size in bytes.
+ * @return USB_SUCCESS on success.
  */
-uint32_t fat_GetFileSize(int8_t fd);
+usb_error_t fat_GetFileSize(int8_t fd, uint32_t *value);
 
 /**
  * Sets the file size.
  * @param path Absolute 8.3 formatted file path.
  * @param size New file size.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void fat_SetFileSize(const char *path, uint32_t size);
+usb_error_t fat_SetFileSize(const char *path, uint32_t size);
 
 /**
  * Gets the current offset in a file.
  * @param fd File descriptor.
- * @return File offset in bytes.
+ * @param offset Returned file offset in bytes.
+ * @return USB_SUCCESS on success.
  */
-uint32_t fat_Tell(int8_t fd);
-
-/**
- * Sets up the buffer used for reading and writing sectors. This buffer must
- * be at least 512 bytes in size.
- * @param buffer Sets the buffer pointer address.
- * @return None.
- */
-void fat_SetBuffer(void *buffer);
+usb_error_t fat_Tell(int8_t fd, uint32_t *offset);
 
 /**
  * Seeks to an arbitrary sector in the flash file. If the sector is beyond
@@ -206,18 +200,18 @@ void fat_SetBuffer(void *buffer);
  * multiple of 512, or 0. Arbitrary seek positions are not supported.
  * @param fd File descriptor
  * @param pos 512 byte multiple offset in file descriptor.
- * @return false if an error occurs.
+ * @return USB_SUCCESS on success.
  */
-bool fat_Seek(int8_t fd, uint32_t pos);
+usb_error_t fat_Seek(int8_t fd, uint32_t pos);
 
 /**
  * Reads the contents of a 512 byte sector of a file into \c fat_SectorBuffer
  * If the current file position results in less than a full 512 byte sector
  * being available, garbage may be read into the excess buffer locations.
  * @param fd File descriptor
- * @return false if an error occurs.
+ * @return USB_SUCCESS on success.
  */
-bool fat_ReadSector(int8_t fd);
+usb_error_t fat_ReadSector(int8_t fd);
 
 /**
  * Writes the contents of the location pointed to by
@@ -225,16 +219,16 @@ bool fat_ReadSector(int8_t fd);
  * The file is extended as needed, however the metadata size of the file
  * needs to be updated using \c fat_SetFileSize.
  * @param fd File descriptor
- * @return false if an error occurs.
+ * @return USB_SUCCESS on success.
  */
-bool fat_WriteSector(int8_t fd);
+usb_error_t fat_WriteSector(int8_t fd);
 
 /**
  * Deletes a file given an absolute path.
  * @param path Absolute 8.3 file path name.
- * @return false if an error occurs.
+ * @return USB_SUCCESS on success.
  */
-bool fat_Delete(const char *path);
+usb_error_t fat_Delete(const char *path);
 
 /**
  * Creates a new file or directory on the device given an absolute path and name.
@@ -242,105 +236,93 @@ bool fat_Delete(const char *path);
  * @param name 8.3 formatted file name.
  * @param attrib FAT style attributes. Can be an combination of \c FAT_RDONLY,
  * \c FAT_HIDDEN, \c FAT_SYSTEM, \c FAT_SUBDIR.
- * @return false if an error occurs.
+ * @return USB_SUCCESS on success.
  */
-bool fat_Create(char *path, char *name, uint8_t attrib);
+usb_error_t fat_Create(const char *path, const char *name, uint8_t attrib);
 
 /**
  * Gets the attributes for a file on the device.
- * @return Returns 255 if no valid stats, otherwise a bitmask of valid FAT
- * attributes.
+ * @param attrib Returned attribute value; 255 if no valid stats, otherwise a
+ * bitmask of valid FAT attributes.
+ * @return USB_SUCCESS on success.
  */
-uint8_t fat_GetAttrib(const char *path);
+usb_error_t fat_GetAttrib(const char *path, uint8_t *attrib);
 
 /**
  * Sets the attributes for a file on the device
  * @param path Absolute 8.3 formatted file path.
  * @param attrib FAT style attributes. Can be an combination of \c FAT_RDONLY,
  * \c FAT_HIDDEN, \c FAT_SYSTEM, \c FAT_SUBDIR.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void fat_SetAttrib(const char *path, uint8_t stat);
+usb_error_t fat_SetAttrib(const char *path, uint8_t attrib);
 
 /**
  * Returns all FAT file / directory entires in a path.
  * @param path Absolute 8.3 formatted root path. Can be NULL for root directory.
  * @param list Storage location for directory entries. Must be pre-allocated.
  * @param directories Only return directories if not 0.
- * @param size Number of allocated \c fat_entry_t available.
+ * @param max Number of allocated \c fat_entry_t available.
  * @param skip Number of entries to skip adding to the list. Useful for memory
  * constraints.
- * @return Number of entries found.
+ * @param num Returned number of entries found.
+ * @return USB_SUCCESS on success.
  */
-int fat_DirList(const char *path, fat_entry_t *list, uint8_t directories, int size, int skip);
+usb_error_t fat_DirList(const char *path, uint8_t directories, int skip, int max, fat_entry_t *list, int *num);
 
 /**
- * Selects the first found attached Mass Storage Device (MSD).
- * This is an alternative to using \c msd_Find and \c msd_Select.
- * Before calling this function, it is important to install an error
- * event handler using the \c msd_SetJmp function.
- * @param ticks Number of 32kHz ticks; timeout before erroring if no devices are detected. (5000 or higher is recommended).
- * @return 0 on success.
+ * Sets up the buffer used for FAT driver handling. This buffer must
+ * be at least 512 bytes in size. It should not be the same buffer as set by
+ * fat_SetDataBuffer(). Other library functions will only use the first 512
+ * bytes of the supplied buffer.
+ * @param buffer Sets the buffer pointer address.
+ * @return None.
  */
-uint8_t msd_Init(unsigned int ticks);
+void msd_SetDriverBuffer(void *buffer);
 
 /**
  * Locates any attached Mass Storage Devices (MSD).
- * @param ms Number of 32kHz ticks; Timeout before erroring if no devices are found.
- * @param result Array of MSD's available, returned from function.
+ * @param ms Number of 32kHz ticks; Timeout before erroring if no devices
+ * are found. Recommended values are > 5000.
+ * @param list Array of MSD's available, returned from function.
  * @param max The maximum number of MSD's that can be found.
- * @warning This function is currently unimplemented. Use \c msd_Init for now.
- * @return The number of attached MSD's.
+ * @param num Returned number of attached MSD's.
+ * @return USB_SUCCESS on success.
  */
-uint8_t msd_Find(unsigned int ticks, msd_t *result, uint8_t max);
+usb_device_t msd_Find(usb_device_t prev, unsigned int ticks);
 
 /**
- * Select an attached Mass Storage Devices (MSD) for use.
- * @param list Array of MSD's available, returned from \c msd_Find.
- * @param index Which attached MSD to use, i.e. '0' would select the first
- * found Mass Storage Device.
- * @warning This function is currently unimplemented. Use \c msd_Init for now.
- * @return None.
+ * Sends a SCSI "TEST_UNIT_READY" packet. This may be needed to periodically
+ * ensure that the device is still alive.
+ * @return USB_SUCCESS on success.
  */
-void msd_Select(msd_t *list, uint8_t index);
+usb_error_t msd_TestUnitReady(void);
 
 /**
- * Sends a SCSI "TEST_UNIT_READY" packet to ensure the device does not fall asleep.
- * This function should be called at least every 2 seconds if the device is idle.
- * @return None.
- */
-void msd_KeepAlive(void);
-
-/**
- * Directly reads a 512 byte sector from the Mass Storage Device
- * @param buffer Pointer to allocated 512 byte buffer to read into.
+ * Directly reads a 512 byte sector from the Mass Storage Device into the
+ * buffer specified by msd_SetDataBuffer(). If the option
+ * MSD_CONFIG_AUTO_ADVANCE_DATA_BUFFER is set, the buffer address is advanced by
+ * 512 bytes.
  * @param sector Logical Block Address (LBA) of sector to read.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void msd_ReadSector(uint8_t *buffer, uint32_t sector);
+usb_error_t msd_ReadSector(uint32_t sector, void *buffer);
 
 /**
- * Directly writes a 512 byte sector to the Mass Storage Device
- * @param buffer Pointer to allocated 512 byte buffer to write.
+* Directly writes a 512 byte sector to the Mass Storage Device from the
+* buffer specified by msd_SetDataBuffer(). If the option
+* MSD_CONFIG_AUTO_ADVANCE_DATA_BUFFER is set, the buffer address is advanced by
+* 512 bytes.
  * @param sector Logical Block Address (LBA) of sector to write.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void msd_WriteSector(uint8_t *buffer, uint32_t sector);
-
-/**
- * Uses a buffer instantiated with \c setjmp as a handle
- * in the event of an error or a detachment of the usb device.
- * @param func Function pointer to call.
- * @return  0 if the first call, otherwise the \c msd_event_t event that
- * occured. You can also trigger this return manually using \c longjmp.
- */
-int msd_SetJmpBuf(jmp_buf env);
+usb_error_t msd_WriteSector(uint32_t sector, void *buffer);
 
 /**
  * Cleans up and deinitializes the current USB device.
- * @return None.
+ * @return USB_SUCCESS on success.
  */
-void msd_Deinit(void);
+usb_error_t msd_Deinit(usb_device_t dev);
 
 #ifdef __cplusplus
 }
